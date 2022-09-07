a784217e712eb9d07d028320550c8827d7e5c00f
nzcsx <vulpes.wang@mail.utoronto.ca>
2022-09-07 02:35:31 -0400

    Update bootstrap_circle to include dir structure creation

--- bootstrap_circle.py
+++ bootstrap_circle.py
@@ -27,6 +27,18 @@ rad_speed = 0.17
 import math
 import os.path
 import numpy as np
+import shutil
+
+
+
+# Make dir structure
+path = os.path.join('Monroe-Datapack-Squid-Workshop-1.19.1','data','monroe','functions','classes','circle')
+if os. path. isdir(path):
+    shutil.rmtree(path)
+
+os.mkdir(os.path.join(path))
+os.mkdir(os.path.join(path,'summon'))
+os.mkdir(os.path.join(path,'travel'))
 
 
 
